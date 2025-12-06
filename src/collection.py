e238f0494c6c0610a6db4deac17b111fd0f01bb4
Andrei Shabanov <86795702+andrei-shabanov@users.noreply.github.com>
2025-12-06 16:06:49 +0100

    def of functions

--- src/collection.py
+++ src/collection.py
@@ -1,21 +1,23 @@
+import pandas as pd
 import re
 from datetime import datetime
-from urllib.parse import urljoin
-from bs4 import BeautifulSoup
-from urllib.parse import urljoin, unquote
-import pandas as pd
 import requests
+from bs4 import BeautifulSoup
+from io import BytesIO
+from urllib.parse import urljoin, unquote
+from pathlib import Path
 
 listOfLinks = []
 
-def collectLinksToYears():
+# retrieves the correct links for each year from the designated page
+def retrieveLinksToYears():
 
     url = 'https://www.monstat.org/eng/page.php?id=180&pageid=44'
     resp = requests.get(url)
     soup = BeautifulSoup(resp.text, "html.parser")
 
     for a in soup.find_all("a", href=True):
-        match = re.search("\d{4}", a.text)
+        match = re.search('\\d{4}', a.text)
         if match:
             print(a.text)
             href = a["href"]
@@ -26,40 +28,60 @@ def collectLinksToYears():
             }
             listOfLinks.append(link)
 
-# collectLinksToYears()
+    print(listOfLinks)
 
-def collectLinks():
+retrieveLinksToYears()
+# collects links to Excel files for each year after the links are retrieved with the collectLinksToYears()
+def retrieveExcelLinks():
     excel_links = []
 
     for link in listOfLinks:
-        if 2016 > int(list(link.values())[0]) > 2013:
-            url = list(link.values())[1]
-            resp = requests.get(url)
+        if int(list(link.values())[0]) > 2013:
+            linkHeader = list(link.values())[1]
+            resp = requests.get(linkHeader)
             soup = BeautifulSoup(resp.text, "html.parser")
 
             for a in soup.find_all("a", href=True):
                 href = a["href"]
                 if href.lower().endswith((".xls", ".xlsx")):
-                    link = urljoin(url, href)
-                    # print(href)
-                    datePart1, datePart2 = unquote(href).split("nglish", 1)
-                    # print(datePart2)
-                    m = re.search(r"([A-Za-z]+)\s+(\d{4})", datePart2)
-                    clean = f"{m.group(1)} {m.group(2)}"
-                    try:
-                        dt = pd.to_datetime(datetime.strptime(clean, "%B %Y").date())
-                    except ValueError:
-                        dt = pd.to_datetime(datetime.strptime(clean, "%b %Y").date())
+                    fullLink = urljoin(linkHeader, href)
+                    print(fullLink)
 
-                    monthLink = {
-                        "date": dt,
-                        "link": link
-                    }
+                    datePart2 = unquote(href)[-40:]
+                    print(datePart2)
 
-                    excel_links.append(monthLink)
-        elif int(list(link.values())[0]) > 2017:
-            url = list(link.values())[1]
+                    if int(list(link.values())[0]) > 2016:
+                        m = re.search(r"\d{4}", datePart2)
+                        clean = f"{m.group()}"
+                        dt = pd.to_datetime(datetime.strptime(clean, "%Y").date())
+                        print(dt)
+
+                        monthLinks = {
+                            "date": dt,
+                            "links": []
+                        }
+
+                        monthLinks["links"].append(fullLink)
+
+                    elif 2016 > int(list(link.values())[0]) > 2013:
+                        m = re.search(r"([A-Za-z]+)\s+(\d{4})", datePart2)
+                        clean = f"{m.group(1)} {m.group(2)}"
+                        try:
+                            dt = pd.to_datetime(datetime.strptime(clean, "%B %Y").date())
+                        except ValueError:
+                            dt = pd.to_datetime(datetime.strptime(clean, "%b %Y").date())
+
+                        monthLinks = {
+                            "date": dt,
+                            "links": []
+                        }
+
+                        monthLinks["links"].append(fullLink)
+
+                    excel_links.append(monthLinks)
+
+
 
     print(excel_links)
 
-# collectLinks()
+retrieveExcelLinks()
